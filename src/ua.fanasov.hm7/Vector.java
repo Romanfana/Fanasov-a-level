79fed1b7bdee6d1b13d1b8cf61ced30d6f042a2f
Romanfana <112722693+Romanfana@users.noreply.github.com>
2022-09-23 11:50:24 +0300

    Add files via upload

---
+++ src/ua.fanasov.hm7/Vector.java
@@ -0,0 +1,80 @@
+public class Vector {
+    public static void main(String[] args) {
+        Vector[] vectors = Vector.generate(10);
+        System.out.println(vectors[0]);
+        System.out.println(vectors[1]);
+        System.out.println(vectors[0].length());
+        System.out.println(vectors[0].scalarProduct(vectors[1]));
+        System.out.println(vectors[0].crossProduct(vectors[1]));
+        System.out.println(vectors[0].cos(vectors[1]));
+        System.out.println(vectors[0].add(vectors[1]));
+        System.out.println(vectors[0].subtract(vectors[1]));
+    }
+
+    private double x;
+    private double y;
+    private double z;
+
+
+    public Vector(double x, double y, double z) {
+        this.x = 3;
+        this.y = 6;
+        this.z = 9;
+    }
+
+
+    public double length() {
+        return Math.sqrt(x * x + y * y + z * z);
+    }
+
+
+    public double scalarProduct(Vector vector) {
+        return x * vector.x + y * vector.y + z * vector.z;
+    }
+
+
+    public Vector crossProduct(Vector vector) {
+        return new Vector(
+                y * vector.z - z * vector.y,
+                z * vector.x - x * vector.z,
+                x * vector.y - y * vector.x);
+    }
+
+
+    public double cos(Vector vector) {
+
+        return scalarProduct(vector) / (length() * vector.length());
+    }
+
+    public Vector add(Vector vector) {
+        return new Vector(
+                x + vector.x,
+                y + vector.y,
+                z + vector.z
+        );
+    }
+
+    public Vector subtract(Vector vector) {
+        return new Vector(
+                x - vector.x,
+                y - vector.y,
+                z - vector.z
+        );
+    }
+
+    public static Vector[] generate(int n) {
+        Vector[] vectors = new Vector[n];
+        for (int i = 0; i < n; i++) {
+            vectors[i] = new Vector(Math.random(), Math.random(), Math.random());
+        }
+        return vectors;
+    }
+
+    public String toString() {
+        return "Vector{" +
+                "x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                '}';
+    }
+}
